97705333d6ddccee3efa5ec914c77c8ea064ced4
Rasquin <yannymk@hotmail.com>
2019-04-04 16:35:32 +0000

    Add dictionary and word searcher

--- test_boggle.py
+++ test_boggle.py
@@ -77,6 +77,45 @@ class TestBoggle(unittest.TestCase):
             others.remove(pos)
             self.assertListEqual(sorted(neighbours[pos]), sorted(others))
         
+    def test_converting_a_path_to_a_word(self):
+        """
+        Ensure that paths can be converted to words
+        """
+        grid = boggle.make_grid(2,2)
+        oneLetterWord = boggle.path_to_word(grid,[(0,0)])
+        twoLetterWord = boggle.path_to_word(grid,[(0,0),(1,1)])
+        self.assertEqual(oneLetterWord, grid[(0,0)])
+        self.assertEqual(twoLetterWord, grid[(0,0)] + grid[(1,1)])
         
         
-    
+    def test_search_grid_for_words(self):
+        """
+        Ensure that certain patterns can be found in a `path_to_word`
+        For this test we will create a mock grid so that we can control the letters.
+        We also define a dictionary of three words. So ab, abc and eee. Two of those
+        words are in the grid and should be found. One of those words is not in the 
+        grid and should not be found. In this case we're using the assert true for 
+        all three of the words 
+        """
+        grid = {(0, 0): 'A', (0, 1): 'B', (1, 0): 'C', (1, 1): 'D'}
+        twoLetterWord = 'AB'
+        threeLetterWord = 'ABC'
+        notThereWord = 'EEE'
+        dictionary = [twoLetterWord, threeLetterWord, notThereWord]
+        
+        foundWords = boggle.search(grid,dictionary)
+        
+        self.assertTrue(twoLetterWord in foundWords)
+        self.assertTrue(threeLetterWord in foundWords)
+        self.assertTrue(notThereWord not in foundWords)
+        
+        
+    def test_load_dictionary(self):
+        """
+        Test that the `get_dictionary` function returns a dictionary
+        that has a length greater than 0
+        """
+        dictionary = boggle.get_dictionary("words.txt")
+        self.assertGreater(len(dictionary), 0)
+        
+        
